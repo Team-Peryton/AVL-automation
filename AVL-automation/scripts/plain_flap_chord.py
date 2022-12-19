c57c83137773b8f6f3a9ff85ab50c7752e4401cc
jj-foster <jonathanfoster46@gmail.com>
2022-12-19 13:31:15 +0000

    control surface sizing

---
+++ AVL-automation/scripts/plain_flap_chord.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from scipy import optimize
+from warnings import filterwarnings
+
+class Iterate():
+    filterwarnings("ignore",message="Covariance of the parameters could not be estimated")
+    
+    def __call__(self,chord_ratio_initial:float,convergence_criteria:float,
+                    angle:float,kf_data:pd.DataFrame,dCl:float,wet_area_ratio:float,
+                    sweep:float):
+        """
+        Calculates chord ratio for a plain flap.
+
+        Arguments:
+            chord_ratio_initial: {float} -- Initial guess for cf/c
+            convergence_criteria: {float} -- 
+            angle: {float} -- Flap angle (degrees)
+            kf_data: {pd.DataFrame} -- Angle, Kf, cf/c plot data (Raymer, 2018)
+            dCl: {float} -- Required delta flap Cl (3D)
+            wet_area_ratio: {float} -- Ratio of wetted area of flap to wing (Raymer, 2018)
+            sweep: {float} -- Surface sweep angle (degrees)
+
+        Returns:
+            chord_ratios: {list[float]} -- List of chord ratios across iterations. [-1] is converged value
+        """
+        
+        return self.iterate(chord_ratio_initial,convergence_criteria,
+                    angle,kf_data,dCl,wet_area_ratio,
+                    sweep)
+    
+    def func_alpha_Kf(self,data,a,b,c,d,e,f,g,h,i):
+        x=data
+        b=1
+        return a*(g*np.arctan((c*x)**d)*(e*x)**f)+(h*x)**i+b
+
+    def curve_fit(self,data,x):
+        np.seterr(all="ignore")
+        parameters,_=optimize.curve_fit(
+            self.func_alpha_Kf,data[:,0],data[:,1],
+            (-0.2, 1, 0.1, 1.5, 0, 1, 1, 0.001, 2)
+        )
+
+        y=self.func_alpha_Kf(x,*parameters)
+
+        np.seterr(all="warn")
+        return y, parameters
+
+    def thickness_ratio_12(self,cl_increment):
+        a=-13.738   
+        b=19.397    
+        c=0.5337-cl_increment   
+
+        return (-b+np.sqrt(b**2-4*a*c))/(2*a)   # quadratic equation
+
+    def iterate(self,chord_ratio_initial:float,convergence_criteria:float,
+                    angle:float,kf_data:pd.DataFrame,dCl:float,wet_area_ratio:float,
+                    sweep:float):
+        
+        ratio_unique=kf_data["cf/c"].unique()
+        ratio_unique.sort()
+        chord_ratio=[chord_ratio_initial]
+        Kf=[]
+        Cl_increment=[]
+        n=0
+        while True:
+    
+            i=np.searchsorted(ratio_unique,chord_ratio[n],side="left")
+            if i==len(ratio_unique):
+                i-=1
+            ratio_left=ratio_unique[i-1]
+            ratio_right=ratio_unique[i]
+
+            data_left=kf_data[kf_data["cf/c"]==ratio_left][["Angle","Kf"]].to_numpy()
+            data_right=kf_data[kf_data["cf/c"]==ratio_right][["Angle","Kf"]].to_numpy()
+
+            Kf_left,parameters_left=self.curve_fit(data_left,angle)
+            Kf_right,parameters_right=self.curve_fit(data_right,angle)
+
+            Kf.append(np.interp(chord_ratio[n],(ratio_left,ratio_right),(Kf_left,Kf_right)))
+            #print(Kf_left,Kf[n],Kf_right)
+
+            Cl_increment.append(dCl/(0.9*Kf[n]*wet_area_ratio*np.deg2rad(angle)*np.cos(np.deg2rad(sweep))))
+            chord_ratio.append(self.thickness_ratio_12(Cl_increment[n]))
+            #print(chord_ratio[n])
+            
+            """ plot for debugging
+            angle_range=np.linspace(10,60,100)
+            kf_left=func_alpha_Kf(angle_range,*parameters_left)
+            kf_right=func_alpha_Kf(angle_range,*parameters_right)
+            
+            fig,ax=plt.subplots()
+            ax.plot(angle_range,kf_left)
+            ax.plot(angle_range,kf_right)
+            ax.scatter(data_left[:,0],data_left[:,1])
+            ax.scatter(data_right[:,0],data_right[:,1])
+
+            ax.scatter((angle,angle,angle),(Kf_left,Kf[n],Kf_right))
+
+            plt.show()
+            """
+
+            #print(chord_ratio[n],chord_ratio[n+1])
+            if np.isclose(chord_ratio[n+1],chord_ratio[n],atol=convergence_criteria):
+                break
+            n+=1
+        
+        return chord_ratio
+
+if __name__=="__main__":
+    kf_data=pd.read_csv("scripts/Kf_plot.csv")
+    angle=30
+    chord_chord_ratio_initial=0.3
+    convergence=0.005
+    dCl=1.4
+    wet_area_ratio=0.9
+    sweep=0
+
+    iterate=Iterate()
+
+    chord_ratios=iterate(chord_chord_ratio_initial,convergence,angle,kf_data,dCl,wet_area_ratio,sweep)
+    print(len(chord_ratios),round(chord_ratios[-1],2))
+
+    # for a in range(15,50,5):
+    #     chord_ratios=iterate(chord_chord_ratio_initial,convergence,a,kf_data,dCl,wet_area_ratio,sweep)
+    #     print(f"{a}: {round(chord_ratios[-1],2)} in {len(chord_ratios)} iterations")
+
---
+++ AVL-automation/scripts/rudder_sizing.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import read_csv
+from tqdm import tqdm
+
+from scripts.plain_flap_chord import Iterate
+from tail_sizing import AutoTail
+
+tail=AutoTail("projects/tail_MDDP_v0.config")
+tail.generate_planes()
+tail.run()
+_,curve_fit=tail.results(display=False)
+
+# setup values
+Lt,St_h,St_v=curve_fit.curve_fit_slice()
+
+kf_data=read_csv("scripts/Kf_plot.csv")
+angle=35
+chord_ratio_initial=0.3
+convergence=0.005
+span_ratio=0.9
+sweep=0
+
+AR_v=5
+thrust_moment_arm=6.83  # m
+thrust_per_side=59400   # N
+x_cg=11.86
+V1=37.597
+
+# find chord ratios
+iterate=Iterate()
+chord_ratios=[]
+#for i,_ in tqdm(enumerate(St_v),total=len(St_v),desc="Calculating cf/c"):
+for i,_ in enumerate(St_v):
+    c_v=np.sqrt(St_v[i]/AR_v)
+    b_v=np.sqrt(St_v[i]*AR_v)
+
+    N=thrust_moment_arm*thrust_per_side
+
+    x_tail=curve_fit.Lt_to_Xt(Lt[i])
+    tail_moment_arm=x_tail-x_cg
+    
+    Lv=N/tail_moment_arm 
+    Clv=Lv/(0.5*1.225*St_v[i]*V1**2)
+
+    r=iterate(chord_ratio_initial,convergence,angle,kf_data,Clv,span_ratio,sweep)
+    chord_ratios.append(round(r[-1],2))
+
+# plot
+fig,ax1,_=curve_fit.plot_slice(Lt,St_h,St_v)
+
+ax2=ax1.twinx()
+ax2.plot(Lt,chord_ratios,color='b',linestyle='--')
+
+ax2.set_ylabel("cf/c")
+
+plt.show()
+
+"""
+cf/c depends only on flap angle & tail volume coefficient
+"""
