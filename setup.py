c0e615fc27dde00deca73d29cf7dc570a8f392b2
jj-foster <jonathanfoster46@gmail.com>
2022-12-23 21:46:37 +0000

    .

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
-import codecs
-import os
 
 VERSION = '0.1'
 DESCRIPTION = "Automation tools for vortex lattice method, AVL"
+LONG_DESCRIPTION = "Automatic tail sizing and dihedral angle investigation made possible through lose wrapping of AVL."
 
 # setup
 setup(
     name="avl_automation",
     version=VERSION,
     author="jj-foster",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     url="https://github.com/Team-Peryton/AVL-automation",
     packages=find_packages("avl_automation"),
     install_requires=["numpy","scipy","pandas","matplotlib","tqdm"],
